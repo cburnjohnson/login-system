de2990c1b9d1a8c8ae76d44bd422ddfa3e12fb8b
Cole Johnson <Colejohnson147@gmail.com>
2020-01-31 00:05:39 -0800

    added jwt

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express();
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -19,7 +20,14 @@ router.post('/', async (req, res) => {
             return;
         }
 
-        res.status(200).json({ message: 'Login Success' });
+        const payload = {
+            user: {
+                id: existsUser.id
+            }
+        };
+
+        const token = jwt.sign(payload, process.env.jwtSecret);
+        res.json(token);
     } catch (err) {
         console.error(err);
         res.status(401).json({ message: err.message });
